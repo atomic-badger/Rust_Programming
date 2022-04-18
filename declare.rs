652b299f5f9c94dd366e378ca9b11f7bb7d5a185
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-04-18 03:24:06 -0400

    Add files via upload

---
+++ declare.rs
@@ -0,0 +1,26 @@
+// DECLARES INTEGERS IN RUST
+
+fn main() {
+    
+    // DECLARE & ASSIGN UNSIGNED 8-BIT INT
+    let var_x :u8 = 9;
+
+    // DECLARE & ASSIGN SIGNED 32-BIT INT
+    let var_y :i32 = 0;
+
+    // DECLARE & ASSIGN  SIGNED 32-BIT INT
+    let  var_z :u32 = 9000;
+    
+    // PRINTS TO CONSOLE THROUGH MACRO
+    println!("\nBEGINNING OF PROGRAM.\n");
+    
+    // PRINTS VARIABLE VALUES TO CONSOLE
+    println!("\nVariable X is: {}", var_x);
+    println!("Variable Y is: {}", var_y);
+    println!("Variable Z is: {}\n", var_z);
+    
+    println!("\nEND OF PROGRAM.\n");
+    
+    return;	//OPTIONAL
+    
+    }
