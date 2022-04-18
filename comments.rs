0f0cabe61707394458e7c07df820c2b05442db3f
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-04-18 03:13:42 -0400

    Add files via upload

---
+++ comments.rs
@@ -0,0 +1,28 @@
+fn main() {
+
+// this is a single line comment.
+// THIS IS ALSO A SINGLE-LINE COMMENT.
+
+// Single-line comments can be used to comment out
+//     single lines of code while debugging programs.
+
+
+// comments should be used to explain code to other humans.
+// remember that someone will be working on this code after you.
+// They may be doing so this month or 50 years from now.
+
+/* 
+	This
+		is
+	 	  a
+	 	   multi-line
+	 				 comment. 
+	 						  */
+	 
+	 							
+/* This is the traditional form of a block-type coment.
+   Block-type comments are useful when commenting out 
+   entire blocks of code. You might want to remember this
+   when you're proofreading or debugging code */
+   							
+}
