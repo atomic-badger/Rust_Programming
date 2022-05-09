70b9fb104f527f3ce7c9f57387bd29e0f86f7e67
milburn brock <96309943+atomic-badger@users.noreply.github.com>
2022-05-09 10:31:18 -0500

    Add files via upload

---
+++ keywords.rs
@@ -0,0 +1,36 @@
+fn main() {
+
+//STATES RUST KEYWORDS
+
+    //HEADER 1
+  println!("\n\tKEYWORDS OF RUST:\n");
+
+  //WORD LIST 1
+  println!("\n\t\tas\t\tfor\t\tSelf\n");
+  println!("\t\tasynch\t\tif\t\tself\n");
+  println!("\t\tawait\t\timpl\t\tstatic\n");
+  println!("\t\tbreak\t\tin\t\tstruct\n");
+  println!("\t\tconst\t\tlet\t\tsuper\n");
+  println!("\t\tcontinue\tloop\t\ttrait\n");
+  println!("\t\tcrate\t\tmatch\t\ttrue\n");
+  println!("\t\tdyn\t\tmod\t\ttype\n");
+  println!("\t\telse\t\tmove\t\tunion\n");
+  println!("\t\tenum\t\tmut\t\tunsafe\n");
+  println!("\t\textern\t\tpub\t\tuse\n");
+  println!("\t\tfalse\t\tref\t\twhere\n");
+  println!("\t\tfn\t\treturn\t\twhile\n");
+
+  //HEADER 2
+  println!("\n\n\tFUTURE RESERVED KEYWORDS:\n");
+
+  //WORD LIST 2
+  println!("\n\t\tabstract\tmacro\t\tunsized\n");
+  println!("\t\tbecome\t\toverride\tvirtual\n");
+  println!("\t\tbox\t\tpriv\t\tyield\n");
+  println!("\t\tdo\t\ttry\n");
+  println!("\t\tfinal\t\ttypeof\n");
+
+  //CLOSER
+  println!("\n\tEND OF RUST KEYWORDS LIST.\n");
+
+}
